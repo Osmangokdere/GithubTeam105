bc8478f0204d1b0ae477c8daf6181da19efe3dcb
Osman Gökdere <osmangokderre@gmail.com>
2023-11-25 08:29:19 +0300

    5.commit

--- src/A04.java
+++ src/A04.java
@@ -1,17 +1,18 @@
 public class A04 {
     public static void main(String[] args) {
         /*
-            bir orojeyi ilk defa olusturdugunuzda
+            Bir projeyi ilk defa olusturdugunuzda;
             1-commit olustur
             2-uzak masaustunde bir repo olutur
             3-github da istenen 3 satiri copy yapip,
             projeye paste yapip calistirir
 
-            Bir projeyi bir kere uzakmasasutu ile isikilendirince
-            sonra yapacagimiz uc degisiklik icin
-            git add.; yeni yapilan degisiklikleri git a ekler
-            2- git commit -m"istenen acıklama" degisiklilieri isteyerek cetrsiyon olusturur
-            3- git push: son versiyonu uzak masaüstüne gönderir
+            Bir projeyi bir kere uzakmasautu ile iliskilendirindikten sonra
+            sonra yapacagimiz uc degisiklik icin;
+
+            1- git add.      : yeni yapilan degisiklikleri git a ekler
+            2- git commit -m :"istenen acıklama" degisiklilieri isteyerek cetrsiyon olusturur
+            3- git push      : son versiyonu uzak masaüstüne gönderir
          */
 
     }
