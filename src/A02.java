3099bccd4975e09a1c11d6e27246d309a81fc51f
Osman Gökdere <osmangokderre@gmail.com>
2023-11-25 07:41:39 +0300

    2.commit

--- src/A02.java
+++ src/A02.java
@@ -1,5 +1,7 @@
 public class A02 {
     public static void main(String[] args) {
         System.out.println("Hello hello");
+        String str="Git komutlari ogrenme";
+        System.out.println(str);
     }
 }
